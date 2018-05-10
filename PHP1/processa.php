db50e69bb3c023aac91c9e6cd53b11ea2fa49e6e
leideFerreira <lmsf@icomp.ufam.edu.br>
2018-05-10 10:48:38 -0400

    PHP1

---
+++ PHP1/processa.php
@@ -0,0 +1,6 @@
+<?php
+	echo "Nome: " .$_GET['nome']. "<br>";
+	echo "E-mail: ".$_GET['email']."<br>";
+	echo "Website: ".$_GET['website']."<br>";
+	echo "Mensagem do contato: "."<br>".$_GET['mensagem']."<br>";
+?>
