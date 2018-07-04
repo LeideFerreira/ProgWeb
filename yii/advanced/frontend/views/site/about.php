e8ffa70daf6cac60660a8b4d539deaa00de749c3
leide ferreira <lmsf@icomp.ufam.edu.br>
2018-07-03 20:47:23 -0400

    Atualiza

--- yii/advanced/frontend/views/site/about.php
+++ yii/advanced/frontend/views/site/about.php
@@ -9,10 +9,9 @@ $this->params['breadcrumbs'][] = $this->title;
 $data =  date("d.m.y h:i:s");
 ?>
 <div class="site-about">
-   
-    <p>This is the About page. You may modify the following file to customize its content:</p><br>
-    <p>Dia e hora atual: <?php echo $data ?></p>
+    <h1><?= Html::encode($this->title) ?></h1>
 
-     
+     <p>This is the About page. You may modify the following file to customize its content:</p><br>
+    <p>Dia e hora atual: <?php echo $data ?></p>
     <code><?= __FILE__ ?></code>
 </div>
